3dcb3d5dd5e5cd4fde4814d8290d70bdbadcb84e
Moises Nascimento <moises.smith759@gmail.com>
2020-03-28 12:22:31 -0300

    att

---
+++ src/uri_exercicios/URI_1259.java
@@ -0,0 +1,33 @@
+package uri_exercicios;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class URI_1259 {
+
+	public static void main(String[] args) {
+
+		java.util.Scanner scan = new java.util.Scanner(System.in);
+		ArrayList<Integer> par = new ArrayList<Integer>();
+		ArrayList<Integer> impar = new ArrayList<Integer>();
+		int N = scan.nextInt();
+		while (N > 0) {
+			int numero = scan.nextInt();
+			if (numero % 2 == 0) {
+				par.add(numero);
+			} else {
+				impar.add(numero);
+			}
+			N--;
+		}
+		Collections.sort(par);
+		Collections.sort(impar, Collections.reverseOrder());
+		for (int i = 0; i < par.size(); i++) {
+			System.out.println(par.get(i));
+		}
+		for (int i = 0; i < impar.size(); i++) {
+			System.out.println(impar.get(i));
+		}
+		scan.close();
+	}
+}
---
+++ src/uri_exercicios/URI_2165.java
@@ -0,0 +1,19 @@
+package uri_exercicios;
+
+public class URI_2165 {
+
+	public static void main(String[] args) {
+
+		java.util.Scanner scan = new java.util.Scanner(System.in);
+
+		String texto = scan.nextLine();
+		char[] arrayChar = texto.toCharArray();
+
+		if (arrayChar.length > 140) {
+			System.out.println("MUTE");
+		} else {
+			System.out.println("TWEET");
+		}
+		scan.close();
+	}
+}
